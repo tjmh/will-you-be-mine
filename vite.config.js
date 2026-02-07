f5efdd2bc7c9ba46fffa1c8a78bd3d7af778abce
Michael He <hemichael98@gmail.com>
2026-02-07 15:33:26 -0800

    Attribute acknowledgments

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import react from "@vitejs/plugin-react";
+import { createRollupLicensePlugin } from "rollup-license-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createRollupLicensePlugin({
+      additionalFiles: {
+        "acknowledgments.json": (packages) => {
+          const favIconAttribution = {
+            name: "favicon.svg",
+            author: "Mihimihi",
+            source: "https://www.flaticon.com/free-icon/love_9812568?term=finger+heart&page=1&position=1&origin=tag&related_id=9812568"
+          };
+
+          packages.unshift(favIconAttribution);
+          return JSON.stringify(packages, null, 2);
+        }
+      }
+    }),
+  ],
   base: "/will-you-be-mine/" 
 })
