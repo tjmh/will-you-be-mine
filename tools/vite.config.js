bcbe36c967034e236b7b79eadc075510d36f031e
Michael He <hemichael98@gmail.com>
2026-02-07 20:25:21 -0800

    test build pipeline

--- tools/vite.config.js
+++ tools/vite.config.js
@@ -4,7 +4,4 @@ import baseConfig from "./vite.config.base.js";
 // https://vite.dev/config/
 export default defineConfig({
     ...baseConfig,
-    define: {
-        "import.meta.env.VITE_GOOGLE_EMBED_API_KEY": `"${process?.env?.GOOGLE_EMBED_API_KEY}"`
-    },
 });
