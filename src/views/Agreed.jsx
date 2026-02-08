afc9f7867b6418babf5add583591018463acf859
Michael He <hemichael98@gmail.com>
2026-02-07 21:49:45 -0800

    Add preview image

--- src/views/Agreed.jsx
+++ src/views/Agreed.jsx
@@ -1,4 +1,7 @@
 import EmbeddedMap from "../components/EmbeddedMap/EmbeddedMap.jsx";
+import PreloadedImage from "../components/PreloadedImage/PreloadedImage.jsx";
+
+import portabellaUrl from "../assets/portabella.png";
 
 import "./Agreed.css";
 
@@ -8,7 +11,10 @@ export default function agreedView() {
             <h1>Yay!</h1>
             <p className="julia-medium-text">I'll see you at Portabella's in Carmel 😊</p>
             <p className="julia-small-text">Location: Ocean Ave, Carmel-By-The-Sea, CA 93921</p>
-            <EmbeddedMap />
+            <div className="preview-container">
+                <PreloadedImage imageUrl={portabellaUrl} />
+                <EmbeddedMap />
+            </div>
         </div>
     )
 }
